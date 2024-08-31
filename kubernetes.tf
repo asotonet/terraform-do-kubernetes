389671344f6345a0e616e2a6f87bfc5c6b05b68b
Alejandro Soto <133695717+asotonet@users.noreply.github.com>
2024-08-31 13:07:50 -0600

    commit

--- kubernetes.tf
+++ kubernetes.tf
@@ -25,7 +25,9 @@ resource "null_resource" "apply_kubectl" {
 #Modifica el tipo de servicio y cambia de tipo cluster a tipo loadbalancer
 resource "null_resource" "patch_argocd_service_kubectl" {
     provisioner "local-exec" {
-    command = "kubectl --kubeconfig=kubeconfig.yaml patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'"
+    command = <<EOT
+kubectl --kubeconfig=kubeconfig.yaml patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+EOT
   }
 
   # Espera a que se halla instalado ArgoCD
@@ -37,7 +39,9 @@ resource "null_resource" "patch_argocd_service_kubectl" {
 #Se obtiene el token admin password para indiicar sesión en Argocd
 resource "null_resource" "get_token_password_kubectl" {
     provisioner "local-exec" {
-    command = "kubectl --kubeconfig=kubeconfig.yaml -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d"
+    command = <<EOT
+kubectl --kubeconfig=kubeconfig.yaml -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+EOT
   }
   depends_on = [
     null_resource.patch_argocd_service_kubectl
