e1d41bcf654b2b812cd0cdd5127c857d55646a72
Alejandro Soto <asoto@aec.co.cr>
2024-08-31 19:44:05 -0600

    Squashed commit of the following:
    commit 397a5e13a8a3a8d9c35cbe65423d00d60b30dfa1
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:23:24 2024 -0600
    
        commit
    
    commit 1d006195c313a26ba4c7001aaa137a217f186f1e
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:21:01 2024 -0600
    
        commit
    
    commit 33d3953fde7b1ad5726aa338f1d1b4c1706fa444
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:19:26 2024 -0600
    
        commit
    
    commit bfc1206f8fa9cc6da02094895dc4b45956d0ff94
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:16:54 2024 -0600
    
        commit
    
    commit b3be64e0aa2d73665753f6584e0bf9b8a20e5bef
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:09:51 2024 -0600
    
        commit
    
    commit 384f075358982eba354360b758b79f1e0a84a13b
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:08:16 2024 -0600
    
        commit
    
    commit 0e2cf70bcab70ba4ebbcd5aef9f17c50ad7cb130
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:04:57 2024 -0600
    
        commit
    
    commit a98caaa2b1b6d24ba0cbdd06d02097dd732b5984
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:02:30 2024 -0600
    
        commit
    
    commit 5d36147e40e84bb6aea4e278346b5761b9ec2f4b
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:01:34 2024 -0600
    
        commit
    
    commit d38348e381eec165d24fd96fbb58d346cd6cd3b4
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 14:00:52 2024 -0600
    
        commit
    
    commit 63960fb12fa51aefca8bfbb65614f82cf7eb1b95
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:59:16 2024 -0600
    
        commit
    
    commit 77f0c843aaed63f135d6eab7cc258057e6703690
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:54:00 2024 -0600
    
        commit
    
    commit ed3cb71ca84e2d47b7d47ff1d1273d8e08945974
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:50:46 2024 -0600
    
        commit
    
    commit cd66be64b1795ecc4fc886acac10ea09f9e822f3
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:49:22 2024 -0600
    
        commit
    
    commit 9259541e6960838b51ce595cc2b08f30b434543e
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:44:18 2024 -0600
    
        commit
    
    commit 9d57828e0087da3ee59fe311b5861734c21af05f
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:23:50 2024 -0600
    
        commit
    
    commit fe5c26af7240eddd149f6c106ba6939d69600ddd
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:09:46 2024 -0600
    
        commit
    
    commit 389671344f6345a0e616e2a6f87bfc5c6b05b68b
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:07:50 2024 -0600
    
        commit
    
    commit 6b8e1fa1a354fb48a40684e1a60ef338c187f6d1
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 13:04:49 2024 -0600
    
        prueba
    
    commit 0ba6c611ee3ddaf46712f9e4ffae8627baa2be42
    Author: Alejandro Soto <133695717+asotonet@users.noreply.github.com>
    Date:   Sat Aug 31 12:59:39 2024 -0600
    
        prueba

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,14 +1,77 @@
+# Se apunta al archivo kubeconfig.yaml
 provider "kubernetes" {
     config_path = "${local_file.kubernetes_config.filename}"  
 }
 
+#Se crea el namespace para argocd
 resource "kubernetes_namespace" "argocd" {
     metadata {
     name = "argocd"
     }
 }
 
+#Se instala argocd
+resource "null_resource" "apply_kubectl" {
+    provisioner "local-exec" {
+    command = "kubectl --kubeconfig=kubeconfig.yaml apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml "
+  }
 
+  # Espera a que se cree el namespace
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
+}
+
+#Modifica el tipo de servicio y cambia de tipo cluster a tipo loadbalancer
+resource "null_resource" "patch_argocd_service_kubectl" {
+    provisioner "local-exec" {
+    command = "kubectl --kubeconfig=kubeconfig.yaml  apply -f ./argocd-config/argocd-server-svc.yaml"
+  }
+
+  # Espera a que se halla instalado ArgoCD
+  depends_on = [
+    null_resource.apply_kubectl
+  ]
+}
+
+/*
+#Se obtiene el token admin password para indiicar sesión en Argocd
+resource "null_resource" "get_token_password_kubectl" {
+    provisioner "local-exec" {
+    command = <<EOT
+kubectl --kubeconfig=kubeconfig.yaml -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+EOT
+  }
+  depends_on = [
+    null_resource.patch_argocd_service_kubectl
+  ]
+}
+*/
+
+#Se trae los datso del SVC con el nombre "argocd"
+data "kubernetes_service" "prueba_svc" {
+  metadata {
+    name      = "argocd-server"
+    namespace = "argocd"
+  }
+}
+# Output para mostrar la IP del LoadBalancer
+output "service_loadbalancer_ip" {
+  value = data.kubernetes_service.prueba_svc.status[0].load_balancer[0].ingress[0].ip
+}
+
+#Se crea el registro DNS para el loadbalancer de Argocd a partir de la IP obtenida en el SVC de loadbalancer creado.
+resource "digitalocean_record" "argocd_dns" {
+  domain = "asntech.lat"
+  type   = "A"
+  name   = "argocd"
+
+  value = data.kubernetes_service.prueba_svc.status[0].load_balancer[0].ingress[0].ip
+
+  depends_on = [
+    null_resource.patch_argocd_service_kubectl
+  ]
+}
 #resource "kubernetes_manifest" "patch_argocd_service" {
 #  manifest = {
 #    "apiVersion" = "v1"
